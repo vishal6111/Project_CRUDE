e004199357f8612e86a20b8b5794d24b6c4eddb4
vishal <vishalkumawat999@gmsil.com>
2021-03-18 00:11:20 +0530

    Project Commit

---
+++ List_DTO/src/DTO/StudentDto.java
@@ -0,0 +1,32 @@
+package DTO;
+
+public class StudentDto {
+	private String id;
+	private String name;
+	private String age;
+	
+	public StudentDto(String id,String name,String age) {
+		this.id=id;
+		this.name=name;
+		this.age=age;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
